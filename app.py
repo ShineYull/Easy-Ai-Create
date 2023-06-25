be65406b20e21584dd802f8cff6965f9e0838c1e
shineyull <shine.yull@foxmail.com>
2023-06-25 11:06:58 +0800

    feat: 添加账号密码登陆。

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import gradio as gr
 def text_handle(name):
     return "Hello " + name + "!"
 
+#账户和密码相同就可以通过
+def same_auth(username, password):
+    return username == password
+
 def ui():
     with gr.Blocks() as interface:
         input_txt = gr.Textbox(label="输入")
@@ -14,7 +18,7 @@ def ui():
             outputs=output_txt,
             api_name="text_api"
         )
-    interface.launch()
+    interface.launch(auth=same_auth,auth_message="username and password must be the same")
 
 if __name__ == "__main__":
     ui()
